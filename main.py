6e4a45e85878805cec87eb3a49b75fef7070bb0d
Kateryna Cherepanova <kateryna.cherepanova.12@gmail.com>
2026-09-10 19:10:18 +0300

    -

--- main.py
+++ main.py
@@ -5,33 +5,36 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
+
     with open("players.json", "r", encoding="utf-8") as file:
         players = json.load(file)
 
     for nickname, player_data in players.items():
-        race_data = player_data["race"]
+        race_data = player_data.get("race")
+
         race, _ = Race.objects.get_or_create(
-            name=race_data["name"],
+            name=race_data.get("name"),
             defaults={
-                "description": race_data["description"]
-            }
+                "description": race_data.get("description"),
+            },
         )
 
-        for skill_data in race_data["skills"]:
+        for skill_data in race_data.get("skills", []):
             Skill.objects.get_or_create(
-                name=skill_data["name"],
+                name=skill_data.get("name"),
                 race=race,
                 defaults={
-                    "bonus": skill_data["bonus"],
+                    "bonus": skill_data.get("bonus"),
                 },
             )
 
-        guild_data = player_data["guild"]
+        guild_data = player_data.get("guild")
+
         if guild_data is not None:
             guild, _ = Guild.objects.get_or_create(
-                name=guild_data["name"],
+                name=guild_data.get("name"),
                 defaults={
-                    "description": guild_data["description"],
+                    "description": guild_data.get("description"),
                 },
             )
         else:
@@ -39,8 +42,8 @@ def main() -> None:
 
         Player.objects.create(
             nickname=nickname,
-            email=player_data["email"],
-            bio=player_data["bio"],
+            email=player_data.get("email"),
+            bio=player_data.get("bio"),
             race=race,
             guild=guild,
         )
